e4ea3b4aa158437064e806a6a724830759063ae6
Kutosi ivan masete <ivanmasete@gmail.com>
2020-01-13 11:48:10 +0000

    Upload New File

---
+++ gallery.js
@@ -0,0 +1,19 @@
+import Navebar from './navebar'; 
+
+
+
+
+
+
+const Gallery=() => (
+
+   
+   <div>  
+      <Navebar/>
+    <h1>These are some of the photos about Akuweera Foundation</h1>
+    <img src="FB.jpg"/>
+</div>
+    
+
+);
+export default Gallery;
