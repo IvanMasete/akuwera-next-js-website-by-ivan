f9cf66da7230c521197d444eff384a082506776f
Kutosi ivan masete <ivanmasete@gmail.com>
2020-01-13 11:54:45 +0000

    Upload New File

---
+++ post.js
@@ -0,0 +1,15 @@
+import { useRouter} from 'next/router';
+import Layout from './myLayout';
+const page = () => {
+    const router = useRouter();
+      
+    return(
+        <Layout>
+            <h1>{router.query.title}</h1>
+            <p>This is mine </p>
+            </Layout>
+
+            
+    );
+};
+export default page;
