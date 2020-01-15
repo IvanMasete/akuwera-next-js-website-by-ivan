ec2606c26c4f50af527391a08751a7a7df14fd96
kaminto <kaminto95@gmail.com>
2020-01-15 17:51:16 +0300

    new date

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
-const withImages = require('next-images')
-module.exports=withImages({})
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS({
+    cssLoaderOptions: {
+        url:false
+    }
+})
